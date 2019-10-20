e7139878432c0587f695aa0547ae44eedeb72f41
Victor Gauthereau <vgauther@student.42.fr>
2019-10-20 18:46:31 +0200

    Init decodeur departement Francais

---
+++ dpt_decodeur.rb
@@ -0,0 +1,215 @@
+def dpt_decodeur(dpt)
+	if (dpt == 1)
+		"Ain - 01"
+	elsif (dpt == 2)
+		"Aisne - 02"
+	elsif (dpt == 3)
+		"Allier - 03"
+	elsif (dpt == 4)
+		"Alpes de Haute Provence - 04"
+	elsif (dpt == 5)
+		"Hautes Alpes - 05"
+	elsif (dpt == 6)
+		"Alpes Maritimes - 06"
+	elsif (dpt == 7)
+		"Ardèche - 07"
+	elsif (dpt == 8)
+		"Ardennes - 08"
+	elsif (dpt == 9)
+		"Ariège - 09"
+	elsif (dpt == 10)
+		"Aube - 10"
+	elsif (dpt == 11)
+		"Aude - 11"
+	elsif (dpt == 12)
+		"Aveyron - 12"
+	elsif (dpt == 13)
+		"Bouches-du-Rhône - 13"
+	elsif (dpt == 14)
+		"Calvados - 14"
+	elsif (dpt == 15)
+		"Cantal - 15"
+	elsif (dpt == 16)
+		"Charente - 16"
+	elsif (dpt == 17)
+		"Charente-maritime - 17"
+	elsif (dpt == 18)
+		"Cher - 18"
+	elsif (dpt == 19)
+		"Corrèze - 19"
+	elsif (dpt == 20)
+		"Corse - 2A et 2B"
+	elsif (dpt == 21)
+		"Côte-d'Or - 21"
+	elsif (dpt == 22)
+		"Côtes-d'Armor - 22"
+	elsif (dpt == 23)
+		"Creuse - 23"
+	elsif (dpt == 24)
+		"Dordogne - 24"
+	elsif (dpt == 25)
+		"Doubs - 25"
+	elsif (dpt == 26)
+		"Drôme - 26"
+	elsif (dpt == 27)
+		"Eure - 27"
+	elsif (dpt == 28)
+		"Eure-et-loir - 28"
+	elsif (dpt == 29)
+		"Finistère - 29"
+	elsif (dpt == 30)
+		"Gard - 30"
+	elsif (dpt == 31)
+		"Haute-garonne - 31"
+	elsif (dpt == 32)
+		"Gers - 32"
+	elsif (dpt == 33)
+		"Gironde - 33"
+	elsif (dpt == 34)
+		"Hérault - 34"
+	elsif (dpt == 35)
+		"Ille-et-vilaine - 35"
+	elsif (dpt == 36)
+		"Indre - 36"
+	elsif (dpt == 37)
+		"Indre-et-loire - 37"
+	elsif (dpt == 38)
+		"Isère - 38"
+	elsif (dpt == 39)
+		"Jura - 39"
+	elsif (dpt == 40)
+		"Landes - 40"
+	elsif (dpt == 41)
+		"Loir-et-cher - 41"
+	elsif (dpt == 42)
+		"Loire - 42"
+	elsif (dpt == 43)
+		"Haute-loire - 43"
+	elsif (dpt == 44)
+		"Loire-atlantique - 44"
+	elsif (dpt == 45)
+		"Loiret - 45"
+	elsif (dpt == 46)
+		"Lot - 46"
+	elsif (dpt == 47)
+		"Lot-et-garonne - 47"
+	elsif (dpt == 48)
+		"Lozère - 48"
+	elsif (dpt == 49)
+		"Maine-et-loire - 49"
+	elsif (dpt == 50)
+		"Manche - 50"
+	elsif (dpt == 51)
+		"Marne - 51"
+	elsif (dpt == 52)
+		"Haute-marne - 52"
+	elsif (dpt == 53)
+		"Mayenne - 53"
+	elsif (dpt == 54)
+		"Meurthe-et-moselle - 54"
+	elsif (dpt == 55)
+		"Meuse - 55"
+	elsif (dpt == 56)
+		"Morbihan - 56"
+	elsif (dpt == 57)
+		"Moselle - 57"
+	elsif (dpt == 58)
+		"Nièvre - 58"
+	elsif (dpt == 59)
+		"Nord - 59"
+	elsif (dpt == 60)
+		"Oise - 60"
+	elsif (dpt == 61)
+		"Orne - 61"
+	elsif (dpt == 62)
+		"Pas-de-calais - 62"
+	elsif (dpt == 63)
+		"Puy-de-dôme - 63"
+	elsif (dpt == 64)
+		"Pyrénées-atlantiques - 64"
+	elsif (dpt == 65)
+		"Hautes-Pyrénées - 65"
+	elsif (dpt == 66)
+		"Pyrénées-orientales - 66"
+	elsif (dpt == 67)
+		"Bas-rhin - 67"
+	elsif (dpt == 68)
+		"Haut-rhin - 68"
+	elsif (dpt == 69)
+		"Rhône - 69"
+	elsif (dpt == 70)
+		"Haute-saône - 70"
+	elsif (dpt == 71)
+		"Saône-et-loire - 71"
+	elsif (dpt == 72)
+		"Sarthe - 72"
+	elsif (dpt == 73)
+		"Savoie - 73"
+	elsif (dpt == 74)
+		"Haute-savoie - 74"
+	elsif (dpt == 75)
+		"Paris - 75"
+	elsif (dpt == 76)
+		"Seine-maritime - 76"
+	elsif (dpt == 77)
+		"Seine-et-marne - 77"
+	elsif (dpt == 78)
+		"Yvelines - 78"
+	elsif (dpt == 79)
+		"Deux-sèvres - 79"
+	elsif (dpt == 80)
+		"Somme - 80"
+	elsif (dpt == 81)
+		"Tarn - 81"
+	elsif (dpt == 82)
+		"Tarn-et-garonne - 82"
+	elsif (dpt == 83)
+		"Var - 83"
+	elsif (dpt == 84)
+		"Vaucluse - 84"
+	elsif (dpt == 85)
+		"Vendée - 85"
+	elsif (dpt == 86)
+		"Vienne - 86"
+	elsif (dpt == 87)
+		"Haute-vienne - 87"
+	elsif (dpt == 88)
+		"Vosges - 88"
+	elsif (dpt == 89)
+		"Yonne - 89"
+	elsif (dpt == 90)
+		"Territoire de belfort - 90"
+	elsif (dpt == 91)
+		"Essonne - 91"
+	elsif (dpt == 92)
+		"Hauts-de-seine - 92"
+	elsif (dpt == 93)
+		"Seine-Saint-Denis - 93"
+	elsif (dpt == 94)
+		"Val-de-marne - 94"
+	elsif (dpt == 95)
+		"Val-d'oise - 95"
+	elsif (dpt == 971)
+		"Guadeloupe - 971"
+	elsif (dpt == 972)
+		"Martinique - 972"
+	elsif (dpt == 973)
+		"Guyane - 973"
+	elsif (dpt == 974)
+		"La réunion - 974"
+	elsif (dpt == 976)
+		"Mayotte - 976"
+	else
+		"Inconnu"
+	end
+end
+
+def main
+n = 1
+	while n < 95
+		puts dpt_decodeur(n)
+		n = n + 1
+	end
+end
+
+main
